e869646a6eaf8bcbd778ffd0cc843d3aa1dfcb29
Harshita <chandaliyaharshita222@gmail.com>
2023-07-13 17:49:05 +0530

    Create a simple stack

---
+++ Stacks_and_Queues/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Stacks_and_Queues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stacks and Queues Problem!");
+            Console.WriteLine("Program to create simple stack");
+            Stack st = new Stack();
+            st.Push(70);
+            st.Push(30);
+            st.Push(56);
+            st.Display();
+        }
+    }
+}
---
+++ Stacks_and_Queues/Stack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks_and_Queues
+{
+    public class Stack
+    {
+        public Node top;
+
+        public void Push(int data)
+        {
+            Node newNode = new Node(data);
+            if (top == null)
+            {
+                top = newNode;
+                Console.WriteLine("{0} is added to stack", newNode.data);
+            }
+            else
+            {
+                newNode.next = top;
+                top = newNode;
+                Console.WriteLine("{0} is added to stack", newNode.data);
+            }
+        }
+
+        public void Display()
+        {
+            if (top == null)
+                Console.WriteLine("No elements are present in stack");
+            else
+            {
+                Node temp = top;
+                Console.WriteLine("\nDisplaying stack elements");
+
+                while (temp != null)
+                {
+                    Console.WriteLine(temp.data + " ");
+                    temp = temp.next;
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
