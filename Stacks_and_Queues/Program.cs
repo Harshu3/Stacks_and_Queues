23f766d32070772dc70d0b0716dcf7379a6e3d2c
Harshita <chandaliyaharshita222@gmail.com>
2023-07-13 18:37:31 +0530

    Create a simple queue

--- Stacks_and_Queues/Program.cs
+++ Stacks_and_Queues/Program.cs
@@ -7,15 +7,12 @@ namespace Stacks_and_Queues
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stacks and Queues Problem!");
-            Console.WriteLine("Program to Peek and Pop from stack till it is empty");
-            Stack st = new Stack();
-            st.Push(70);
-            st.Push(30);
-            st.Push(56);
-            st.Display();
-            Console.WriteLine("\nRemoving all elements till stack is empty\n");
-            st.IsEmpty();
-            st.Display();
+            Console.WriteLine("Program to create a simple queue");
+            Queue queue = new Queue();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.Display();
         }
     }
 }
---
+++ Stacks_and_Queues/Queue.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks_and_Queues
+{
+    public class Queue
+    {
+        Node top = null;
+
+        public void Enqueue(int data)
+        {
+            Node newNode = new Node(data);
+            if (top == null)
+            {
+                top = newNode;
+                Console.WriteLine("{0} is added to queue", newNode.data);
+            }
+            else
+            {
+                Node temp = top;
+                while (temp.next != null)
+                    temp = temp.next;
+
+                temp.next = newNode;
+                Console.WriteLine("{0} is added to queue", newNode.data);
+            }
+        }
+
+        public void Display()
+        {
+            Node temp = top;
+
+            if (top == null)
+                Console.WriteLine("No elements are present in queue");
+            
+            Console.WriteLine("\nDisplaying queue elements");
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
